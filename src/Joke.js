6b174ae692a5c377c6148aa27e490fef6233b3ec
nll004 <loninath17@gmail.com>
2022-11-09 07:54:11 -0500

    switched joke component from function to class based as project details

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,33 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
+  }
 
-  return (
-    <div className="Joke">
+  upVote = () => this.props.vote(this.props.id, +1);
+
+  downVote = () => this.props.vote(this.props.id, -1);
+
+  render(){
+    return (
+      <div className="Joke">
       <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
-
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
-
-        {votes}
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+        {this.props.votes}
       </div>
-
-      <div className="Joke-text">{text}</div>
+      <div className="Joke-text">{this.props.text}</div>
     </div>
-  );
+    )
+  }
 }
 
 export default Joke;
